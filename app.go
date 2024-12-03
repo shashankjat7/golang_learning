7d0034ba68a8e473f3849272a467e056a0ec427e
shashankjat7 <shashankjat7@gmail.com>
2024-12-03 15:14:41 +0530

    functions

--- app.go
+++ app.go
@@ -7,16 +7,22 @@ func main() {
 	var expenses float64
 	var taxRate float64
 	// get the relevant data
-	fmt.Print("Enter your company's revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter your company's expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter the tax rate: ")
-	fmt.Scan(&taxRate)
+	revenue, expenses, taxRate = getTheNumbers()
 	ebt := revenue - expenses
 	profit := ebt - (ebt * taxRate / 100)
 	ratio := ebt / profit
 	fmt.Println(ebt)
 	fmt.Println(profit)
 	fmt.Println(ratio)
+}
+
+// getting all the input data from the user
+func getTheNumbers() (revenue float64, expenses float64, taxRate float64) {
+	fmt.Print("Enter your company's revenue: ")
+	fmt.Scan(&revenue)
+	fmt.Print("Enter your company's expenses: ")
+	fmt.Scan(&expenses)
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&taxRate)
+	return
 }
