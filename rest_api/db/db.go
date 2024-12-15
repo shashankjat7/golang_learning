d5732727cc3d13ad8e84993a45da24e1491fa5f7
shashankjat7 <shashankjat7@gmail.com>
2024-12-15 21:46:50 +0530

    adding db config

--- rest_api/db/db.go
+++ rest_api/db/db.go
@@ -2,9 +2,45 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func InitDB(){
-	sql.Open("sqlite3")
+// a global variable for databse
+var DB *sql.DB
+
+// initialize the database
+func InitDB() {
+	var err error
+	DB, err := sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	// how many connections should be open
+	DB.SetMaxOpenConns(10)
+	// how many max connections can be idle
+	DB.SetMaxIdleConns(5)
+
+	// created table
+	createTables()
+}
+
+func createTables() {
+	createEventsTableQuery := `
+	CRREATE TABLE IF NOT EXISTS events (
+		id TEXT PRIMARY KEY AUTOINCREMENT
+		title TEXT NOT NULL,
+		description TEXT NOT NULL,
+		location TEXT NOT NULL,
+		start_time DATETIME NOT NULL,
+		created_by TEXT NOT NULL
+	)
+	`
+	_, err := DB.Exec(createEventsTableQuery)
+	if err != nil {
+		panic(fmt.Sprint("An error occurred while creating the table", err))
+	}
 }
